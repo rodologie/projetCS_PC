c6f4621f6b0816c90ccedee64e2784c16d67cf01
Rodolphe Lajugie <rodolphe.lajugie@cpe.fr>
2022-06-13 13:49:08 +0200

    ajouts

--- prog.py
+++ prog.py
@@ -2,13 +2,19 @@ import sys, os
 
 
 fct = input('Quel fichier voulez-vous executer ?')
+
+lst_FCT=['Calcul_PI.py']
 lst=[fct,'.py']
 file = ''.join(lst)
 
-print("vous executer l'exercice %s" %file)
+
 pid=os.fork()
 if pid == 0:
-    os.execlp('python3', 'python3', file )
+    if file in lst_FCT:
+        print("vous executer l'exercice %s" %file)
+        os.execlp('python3', 'python3', file )
+    else:
+        print("Fichiers introuvable, vérifier l'orthographe")
 else:
     os.wait()
-    sys.exit(1)
+    sys.exit(0)
