3a739d4d3c393e3e499349e87e1cd7b508014f7b
Valentin <valbrqd@gmail.com>
2022-06-17 16:56:12 +0200

    ok

--- Calculs.py
+++ Calculs.py
@@ -7,17 +7,16 @@ import random as rd
 
 lstOpe=[]
 
-if len(sys.argv)<2:
-    print('Pas assez dargument')
-elif len(sys.argv[1:])< int(sys.argv[1]) :
-    print('Pas assez de calcul')
-else :
-    lstOpe.append(sys.argv[1:])
+lstArg = []
+lstArg.append(int(input('Combien de demandeur ? ')))
 
+for i in range(lstArg[0]):
+    cbcalc = int(input('Combien de calcul pour le demandeur '))
+    lstArg.append(cbcalc)
+    
+def demande(id): 
 
-def demande(id): #nbc = nombre de calcule a faire
-
-    for i in range(int(sys.argv[int(id)+2])):
+    for i in range(lstArg[int(id)+1]):
         a = rd.randint(1,10)
         b = rd.randint(1,10)
         opp = rd.choice(['+','/','-','*'])
@@ -46,7 +45,7 @@ def calculateur(id):
     calc1 = entree.get()
     fin_calc = str(id) + ' et je trouve ça : ' +  str(eval(calc1[1:]))
     
-    #print('Je calcule la demande de ' ,fin_calc)
+
     
     sortie.put(fin_calc)
     verrou.release()
@@ -58,7 +57,7 @@ if __name__ == '__main__':
     verrou = mp.Lock()
     dem = []
 
-    for i in range(int(sys.argv[1])):
+    for i in range(lstArg[0]):
         dem.append(mp.Process(target = demande,args=(str(i))))
     for proc in dem :
         proc.start()
